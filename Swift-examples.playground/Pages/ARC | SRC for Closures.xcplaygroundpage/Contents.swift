250f64693410421e07bb467c0dabe0f790958e45
Beka-l3 <b.talgat@innopolis.university>
2023-09-27 12:38:31 +0300

    SRC for Closure

--- Swift-examples.playground/Pages/ARC | SRC for Closures.xcplaygroundpage/Contents.swift
+++ Swift-examples.playground/Pages/ARC | SRC for Closures.xcplaygroundpage/Contents.swift
@@ -83,13 +83,74 @@ print("To prevent this by defining a capture list")
 /// whenever you refer to a member of self within a closure.
 /// This helps you remember that it’s possible to capture self by accident.
 
+print("\n\n")
 
 // MARK: - Resolve SRC
+protocol SomeDelegate: AnyObject { }
+
+final class SomeClass {
+    
+    var delegate: SomeDelegate?
+    
+    /// Each item in a capture list is a pairing of the weak or unowned keyword with a reference to a class instance (such as self)
+    /// or a variable initialized with some value (such as delegate = self.delegate).
+    /// These pairings are written within a pair of square braces, separated by commas.
+    lazy var someClosure = { [unowned self, weak delegate = self.delegate] (index: Int, stringToProcess: String) -> String in
+        
+        return ""
+    }
+    
+    /// if a closure doesn’t specify a parameter list or return type because they can be inferred from context,
+    /// place the capture list at the very start of the closure, followed by the `in` keyword:
+    lazy var someClosure2 = { [unowned self, weak delegate = self.delegate] in
+        
+    }
+    
+    init() {
+        
+    }
+    
+}
 
 
+class HTMLElement2 {
 
+    let name: String
+    let text: String?
 
+    /// If the captured reference will never become `nil`,
+    /// it should always be captured as an unowned reference, rather than a weak reference.
+    lazy var asHTML: () -> String = { [unowned self] in
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "<\(self.name) />"
+        }
+    }
 
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+var paragraph2: HTMLElement2? = HTMLElement2(name: "p", text: "hello, world")
+print(paragraph2!.asHTML())
+
+print("Current refferences look like this\n")
+
+print(" paragraph -----> p: hellow world          ")
+print("                     |     ^               ")
+print("                     |     :               ")
+print("                     ˇ     :               ")
+print("                closure: () -> String      ")
+print("\n")
+
+paragraph2 = nil
 
 
 
