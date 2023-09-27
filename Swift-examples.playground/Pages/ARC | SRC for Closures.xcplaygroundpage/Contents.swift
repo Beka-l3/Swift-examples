443d991e77aecbe67b6aeaadfcfa34977dbe2cf1
Beka-l3 <b.talgat@innopolis.university>
2023-09-27 12:27:51 +0300

    SRC for Closure

--- Swift-examples.playground/Pages/ARC | SRC for Closures.xcplaygroundpage/Contents.swift
+++ Swift-examples.playground/Pages/ARC | SRC for Closures.xcplaygroundpage/Contents.swift
@@ -8,5 +8,90 @@ import Foundation
 ///
 /// Closures are `reference types`
 
+class HTMLElement {
+
+    let name: String
+    let text: String?
+
+    lazy var asHTML: () -> String = {
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "<\(self.name) />"
+        }
+    }
+
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+
+}
+
+/// The asHTML property is named and used somewhat like an instance method.
+/// However, because asHTML is a closure property rather than an instance method,
+/// you can replace the default value of the asHTML property with a custom closure,
+/// if you want to change the HTML rendering for a particular HTML element.
+
+/// example
+
+let heading = HTMLElement(name: "h1")
+let defaultText = "some default text"
+heading.asHTML = {
+    return "<\(heading.name)>\(heading.text ?? defaultText)</\(heading.name)>"
+}
+print(heading.asHTML())
+
+
+/// The asHTML property is declared as a lazy property,
+/// because it’s only needed if and when the element actually needs to be rendered as a string value for some HTML output target.
+/// The fact that asHTML is a lazy property means that you can refer to self within the default closure,
+/// because the lazy property will not be accessed until after initialization has been completed and self is known to exist.
+
+
+var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello, world")
+print(paragraph!.asHTML())
+
+print("Above codes create SRC")
+print("Take var pragraph as an example\n")
+
+
+print("Current refferences look like this\n")
+
+print(" paragraph -----> p: hellow world          ")
+print("                     |     ^               ")
+print("                     |     |               ")
+print("                     ˇ     |               ")
+print("                closure: () -> String      ")
+print("\n")
+
+paragraph = nil
+
+print("Dinit is not called because of SRC\n")
+
+print("To prevent this by defining a capture list")
+
+/// As with strong reference cycles between two class instances,
+/// you declare each captured reference to be a weak or unowned reference rather than a strong reference.
+/// The appropriate choice of weak or unowned depends on the relationships between the different parts of your code.
+
+/// Swift requires you to write self.someProperty or self.someMethod() (rather than just someProperty or someMethod())
+/// whenever you refer to a member of self within a closure.
+/// This helps you remember that it’s possible to capture self by accident.
+
+
+// MARK: - Resolve SRC
+
+
+
+
+
+
+
+
 
 
