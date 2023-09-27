26e65f777ebfd3595b1e51f6f8190e422b81b998
Beka-l3 <b.talgat@innopolis.university>
2023-09-27 13:02:39 +0300

    Structures and Classes

---
+++ Swift-examples.playground/Pages/Structures & Classes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+/// Structures and Enumerations are `value types`
+/// A value type is a type whose value is copied when it’s assigned to a variable or constant, or when it’s passed to a function.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
