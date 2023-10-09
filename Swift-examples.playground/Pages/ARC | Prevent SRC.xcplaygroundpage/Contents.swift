a7b5ae52faafb99deda6374aeda419e9f13e8be8
Beka-l3 <b.talgat@innopolis.university>
2023-10-09 11:40:47 +0300

    Little refactoring

--- Swift-examples.playground/Pages/ARC | Prevent SRC.xcplaygroundpage/Contents.swift
+++ Swift-examples.playground/Pages/ARC | Prevent SRC.xcplaygroundpage/Contents.swift
@@ -3,14 +3,14 @@ import Foundation
 /// There are 2 ways to prevent Strong Reference Cycle
 /// ``weak`` `var`
 /// ``unowned``  `var`
-/// They both are the mechanisms that ``does not increase`` `Reference Count (RC)`
+/// They both are the mechanisms that ``does not increase`` ` Reference Count (RC)`
 /// The difference is that ``unowned`` guaranties that the object is not `nil`
 /// If the object is deinited then ``weak`` points to `nil`
 /// however ``unowned`` will cause `runtime error`
-/// Use a weak reference when the other instance has a shorter lifetime — that is, when the other instance can be deallocated first.
-/// Use an unowned reference when the other instance has the same lifetime or a longer lifetime.
+/// Use a ``weak`` reference when the other instance has a `shorter` `lifetime` — that is, when the other instance can be deallocated first.
+/// Use an ``unowned`` reference when the other instance has the `same` `lifetime` or a `longer` `lifetime`.
 
-/// ``Weak`` Property observers aren’t called when ARC sets a weak reference to nil.
+/// ``Weak`` Property observers aren’t called when `ARC` sets a weak reference to `nil`.
 
 // MARK: - Method 1      weak
 final class Person {
@@ -47,93 +47,92 @@ final class Baloon {
 
 
 /// example 1
-var p1: Person? = Person(name: "Bekzhan")
-var i1: Baloon? = Baloon(color: .red)
-print()
-
-print("Current refferences look like this\n")
-
-print(" p1 ----->  Bekzhan       ")
-print("                          ")
-print(" i1 ----->  Red Balooon   ")
-print("\n")
-
-
-p1?.itemOnHand = i1
-i1?.owner = p1
-print()
-
-print("Current refferences look like this\n")
-
-print(" p1 ----->  Bekzhan       ")
-print("            |   ^         ")
-print("            |   :         ")
-print("            |   :         ")
-print("            ˇ   :         ")
-print(" i1 ----->  Red Balooon   ")
-print("\n")
-
-
-i1 = nil
-print("Now i1 points to 'nil'")
-print("p1 points to Bekzhan")
-print("Bekzhan's itemOnHand points to Read Baloon")
-print("Read Baloon's owner is weak var points to Bekzhan")
-print("But weak reference does not increase RC")
-print("So Bekzhan's RC == 1")
-print("And Red Baloon's RC == 1\n")
-
-print("Current refferences look like this\n")
-
-print(" p1 ----->  Bekzhan       ")
-print("            |   ^         ")
-print("            |   :         ")
-print("            |   :         ")
-print("            ˇ   :         ")
-print(" i1         Red Balooon   ")
-print("\n")
-
-p1 = nil
-print()
-print("Current refferences look like this\n")
-
-print(" p1         Bekzhan       ")
-print("                          ")
-print(" i1         Red Balooon   ")
-print("\n\n")
+func foo1() {
+    var p1: Person? = Person(name: "Bekzhan")
+    var i1: Baloon? = Baloon(color: .red)
+    print()
+    
+    print("Current refferences look like this\n")
+    print(" p1 ----->  Bekzhan       ")
+    print("                          ")
+    print(" i1 ----->  Red Balooon   ")
+    print("\n\n")
+    
+    
+    p1?.itemOnHand = i1
+    i1?.owner = p1
+    
+    print("Current refferences look like this\n")
+    print(" p1 ----->  Bekzhan       ")
+    print("            |   ^         ")
+    print("            |   :         ")
+    print("            |   :         ")
+    print("            ˇ   :         ")
+    print(" i1 ----->  Red Balooon   ")
+    print("\n\n")
+    
+    
+    i1 = nil
+    print("Now i1 points to 'nil'")
+    print("p1 points to Bekzhan")
+    print("Bekzhan's itemOnHand points to Read Baloon")
+    print("Read Baloon's owner is weak var points to Bekzhan")
+    print("But weak reference does not increase RC")
+    print("So Bekzhan's RC == 1")
+    print("And Red Baloon's RC == 1\n")
+    
+    print("Current refferences look like this\n")
+    print(" p1 ----->  Bekzhan       ")
+    print("            |   ^         ")
+    print("            |   :         ")
+    print("            |   :         ")
+    print("            ˇ   :         ")
+    print(" i1         Red Balooon   ")
+    print("\n\n")
+    
+    p1 = nil
+    print("Current refferences look like this\n")
+    print(" p1         Bekzhan       ")
+    print("                          ")
+    print(" i1         Red Balooon   ")
+    print("\n\n")
+}
+//foo1()
 
 
 /// example 2
-var p2: Person? = Person(name: "Zhangir")
-var i2: Baloon? = Baloon(color: .blue)
-p2?.itemOnHand = i2
-i2?.owner = p2
-print()
-
-print("Current refferences look like this\n")
-
-print(" p1 ----->  Zhangir       ")
-print("            |   ^         ")
-print("            |   :         ")
-print("            |   :         ")
-print("            ˇ   :         ")
-print(" i1 ----->  Blue Balooon  ")
-print("\n")
-
-p2 = nil
-print()
-
-print("Since weak does not increase RC")
-print("When p2 is set to nil")
-print("Zhangir has been deinted immediately")
-print("And Blue Baloon's owner is nil, too'n")
-
-print("Current refferences look like this\n")
-
-print(" p1         Zhangir       ")
-print("                          ")
-print(" i1 ----->  Blue Balooon  ")
-print("\n")
+func foo2() {
+    var p2: Person? = Person(name: "Zhangir")
+    var i2: Baloon? = Baloon(color: .blue)
+    p2?.itemOnHand = i2
+    i2?.owner = p2
+    
+    print("Current refferences look like this\n")
+    print(" p2 ----->  Zhangir       ")
+    print("            |   ^         ")
+    print("            |   :         ")
+    print("            |   :         ")
+    print("            ˇ   :         ")
+    print(" i2 ----->  Blue Balooon  ")
+    print("\n\n")
+    
+    print(i2?.owner ?? "some")
+    p2 = nil
+    print(i2?.owner ?? "nil", "\n\n")
+    
+    
+    print("Since weak does not increase RC")
+    print("When p2 is set to nil")
+    print("Zhangir has been deinted immediately")
+    print("And Blue Baloon's owner is nil, too'n")
+    
+    print("Current refferences look like this\n")
+    print(" p2         Zhangir       ")
+    print("                          ")
+    print(" i2 ----->  Blue Balooon  ")
+    print("\n\n")
+}
+//foo2()
 
 
 // MARK: - Method 2         unowned
@@ -169,22 +168,27 @@ final class CreditCard {
     deinit { print("Card lost it's owner: #\(number)") }
 }
 
-var beka: Customer?
-beka = Customer(name: "Bekzhan Talgat")
-beka!.card = CreditCard(number: 1234_5678_9012_3456, customer: beka!)
+func foo3() {
+    var beka: Customer?
+    beka = Customer(name: "Bekzhan Talgat")
+    beka!.card = CreditCard(number: 1234_5678_9012_3456, customer: beka!)
+    
+    print("Current refferences look like this\n")
+    
+    print(" p1 -----> Beka           ")
+    print("           |  ^           ")
+    print("           |  :           ")
+    print("           ˇ  :           ")
+    print("           Card           ")
+    print("\n")
+    
+    beka = nil
+    
+    print("In CreditCards deinit, we cannot access customer, because it is already deinited\n")
+    print("\n")
+}
+//foo3()
 
-print("Current refferences look like this\n")
-
-print(" p1 -----> Beka           ")
-print("           |  ^           ")
-print("           |  :           ")
-print("           ˇ  :           ")
-print("           Card           ")
-print("\n")
-
-beka = nil
-
-print("In CreditCards deinit, we cannot access customer, because it is already deinited")
 
 /// example 2
 /// `unowned` can be also a `var` and `Optional`
@@ -211,17 +215,23 @@ class Course {
 
 
 /// example 3
-/// `unowned` can be `Implicitly Unwrapped Optiona`
+/// `unowned` can be `Implicitly Unwrapped Optional`
 /// It will be like a just `Optional` and default value is `nil`
-/// This means that the capitalCity property has a default value of `nil`,
-/// like any other` optional`,
+/// This means that the `capitalCity` property has a default value of `nil`,
+/// like any other `optional`,
 /// but can be accessed without the need to unwrap its value as described in
 class Country {
     let name: String
     var capitalCity: City!
+    
     init(name: String, capitalName: String) {
         self.name = name
         self.capitalCity = City(name: capitalName, country: self)
+        print("Country is inited")
+    }
+    
+    deinit {
+        print("Country is deinited")
     }
 }
 
@@ -229,8 +239,27 @@ class Country {
 class City {
     let name: String
     unowned let country: Country
+    
     init(name: String, country: Country) {
         self.name = name
         self.country = country
+        print("City is inited")
+    }
+    
+    deinit {
+        print("City is deinited")
     }
 }
+
+func foo5() {
+    var cntr = Country(name: "KZ", capitalName: "Satana")
+}
+//foo5()
+var cntr: Country? = Country(name: "KZ", capitalName: "Satana")
+
+print(cntr!.capitalCity.name)
+cntr!.capitalCity = nil
+print(cntr!.capitalCity ?? "nil")
+cntr = nil
+
+
--- Swift-examples.playground/Pages/ARC | SRC for Closures.xcplaygroundpage/Contents.swift
+++ Swift-examples.playground/Pages/ARC | SRC for Closures.xcplaygroundpage/Contents.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-/// A strong reference cycle can also occur
-/// if you assign a closure to a property of a class instance,
-/// and the body of that closure captures the `instance`.
+/// A `strong reference cycle` can also occur
+/// if you assign a `closure` to a property of a `class instance`,
+/// and the body of that closure ``captures`` the `instance`.
 /// This capture might occur because the closure’s body accesses a property of the instance, such as `self.someProperty`
 /// or because the closure calls a method on the instance, such as `self.someMethod()`
 ///
-/// Closures are `reference types`
+/// Closures are ``reference types``
 
 class HTMLElement {
 
@@ -32,58 +32,64 @@ class HTMLElement {
 
 }
 
-/// The asHTML property is named and used somewhat like an instance method.
-/// However, because asHTML is a closure property rather than an instance method,
-/// you can replace the default value of the asHTML property with a custom closure,
-/// if you want to change the HTML rendering for a particular HTML element.
+/// The `asHTML` property is named and used somewhat like an instance method.
+/// However, because `asHTML` is a closure property rather than an instance method,
+/// you can replace the default value of the `asHTML` property with a custom closure,
+/// if you want to change the `HTML` rendering for a particular `HTML` element.
 
 /// example
-
-let heading = HTMLElement(name: "h1")
-let defaultText = "some default text"
-heading.asHTML = {
-    return "<\(heading.name)>\(heading.text ?? defaultText)</\(heading.name)>"
+func foo1() {
+    var heading = HTMLElement(name: "h1")
+    let defaultText = "some default text"
+    heading.asHTML = {
+        return "<\(heading.name)>\(heading.text ?? defaultText)</\(heading.name)>"
+    }
+    print(heading.asHTML())
+    
+    /// The `asHTML` property is declared as a `lazy` property,
+    /// because it’s `only needed` if and when the element actually needs to be rendered as a string value for some HTML output target.
+    /// The fact that `asHTML` is a `lazy` property means that you can refer to `self` `within the default closure`,
+    /// because the `lazy` property will not be accessed `until` after initialization has been completed and `self` is known to `exist`.
+    
+    print("\n")
+    
+    
+    func foo() {
+        var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello, world")
+        print(paragraph!.asHTML(), "\n")
+        
+        print("Above codes create SRC")
+        print("Take var pragraph as an example\n")
+        
+        
+        print("Current refferences look like this\n")
+        
+        print(" paragraph -----> p: hellow world          ")
+        print("                     |     ^               ")
+        print("                     |     |               ")
+        print("                     ˇ     |               ")
+        print("                closure: () -> String      ")
+        print("\n")
+        
+        paragraph = nil
+        
+        print("Dinit is not called because of SRC\n")
+        
+        print("Prevent this by defining a capture list")
+        
+        /// As with strong reference cycles between two class instances,
+        /// you declare each captured reference to be a weak or unowned reference rather than a strong reference.
+        /// The appropriate choice of weak or unowned depends on the relationships between the different parts of your code.
+        
+        /// Swift requires you to write self.someProperty or self.someMethod() (rather than just someProperty or someMethod())
+        /// whenever you refer to a member of self within a closure.
+        /// This helps you remember that it’s possible to capture self by accident.
+        print("\n")
+    }
+    foo()
+    print("\n")
 }
-print(heading.asHTML())
-
-
-/// The asHTML property is declared as a lazy property,
-/// because it’s only needed if and when the element actually needs to be rendered as a string value for some HTML output target.
-/// The fact that asHTML is a lazy property means that you can refer to self within the default closure,
-/// because the lazy property will not be accessed until after initialization has been completed and self is known to exist.
-
-
-var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello, world")
-print(paragraph!.asHTML())
-
-print("Above codes create SRC")
-print("Take var pragraph as an example\n")
-
-
-print("Current refferences look like this\n")
-
-print(" paragraph -----> p: hellow world          ")
-print("                     |     ^               ")
-print("                     |     |               ")
-print("                     ˇ     |               ")
-print("                closure: () -> String      ")
-print("\n")
-
-paragraph = nil
-
-print("Dinit is not called because of SRC\n")
-
-print("To prevent this by defining a capture list")
-
-/// As with strong reference cycles between two class instances,
-/// you declare each captured reference to be a weak or unowned reference rather than a strong reference.
-/// The appropriate choice of weak or unowned depends on the relationships between the different parts of your code.
-
-/// Swift requires you to write self.someProperty or self.someMethod() (rather than just someProperty or someMethod())
-/// whenever you refer to a member of self within a closure.
-/// This helps you remember that it’s possible to capture self by accident.
-
-print("\n\n")
+foo1()
 
 // MARK: - Resolve SRC
 protocol SomeDelegate: AnyObject { }
