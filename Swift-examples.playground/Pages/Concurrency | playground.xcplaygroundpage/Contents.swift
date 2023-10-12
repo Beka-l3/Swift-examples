12850befb24bca64508151e0c5db20b7e4a6701e
Beka-l3 <b.talgat@innopolis.university>
2023-10-12 16:28:54 +0300

    Setup table view

--- Swift-examples.playground/Pages/Concurrency | playground.xcplaygroundpage/Contents.swift
+++ Swift-examples.playground/Pages/Concurrency | playground.xcplaygroundpage/Contents.swift
@@ -1,7 +1,6 @@
 import UIKit
 import PlaygroundSupport
 
-//PlaygroundPage.current.liveView = MyVC()
 //let url = URL(staticString: "https://source.unsplash.com/random")
 
 struct Movie {
@@ -48,6 +47,9 @@ final class MyVC: UIViewController {
 //    MARK:  private func
     private func setupViews() {
         viewComponents.setupViews(parent: view)
+        
+        viewComponents.tableView.delegate = self
+        viewComponents.tableView.dataSource = self
     }
 }
 
@@ -57,12 +59,14 @@ extension MyVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        ratedMovies.count
+//        ratedMovies.count
+        10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: MyCell.reuseIdentifier) as! MyCell
-        cell.setData(movie: ratedMovies[indexPath.row])
+//        cell.setData(movie: ratedMovies[indexPath.row])
+        cell.title = "\(indexPath.row)"
         return cell
     }
 }
@@ -73,7 +77,7 @@ final class MyVCViewComponents {
     
     lazy var tableView: UITableView = {
         let table = UITableView()
-        
+        table.register(MyCell.self, forCellReuseIdentifier: MyCell.reuseIdentifier)
         table.translatesAutoresizingMaskIntoConstraints = false
         return table
     }()
@@ -81,9 +85,16 @@ final class MyVCViewComponents {
     
 //    MARK:  func
     func setupViews(parent: UIView) {
+        parent.backgroundColor = .systemPurple
+        parent.addSubview(tableView)
         
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: parent.safeAreaLayoutGuide.topAnchor, constant: 16),
+            tableView.leadingAnchor.constraint(equalTo: parent.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            tableView.trailingAnchor.constraint(equalTo: parent.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            tableView.bottomAnchor.constraint(equalTo: parent.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+        ])
     }
-    
 }
 
 
@@ -96,23 +107,27 @@ final class MyCell: UITableViewCell {
         static let height: CGFloat = Design.HIG.tableCellHeight
     }
     
+    private let viewComponents: MyCellViewComponents = .init()
     
-//    MARK: UIKit
-    private lazy var poster: UIImageView = {
-        let view = UIImageView()
-        
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
+    var title: String {
+        get { viewComponents.titleLabel.text ?? .empty }
+        set { viewComponents.titleLabel.text = newValue }
+    }
     
 
 //    MARK:  lifecycle
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
     }
     
     
@@ -123,7 +138,52 @@ final class MyCell: UITableViewCell {
     
     
 //    MARK:  private func
-    private func setupView() {
-        
+    private func setupViews() {
+        viewComponents.setupViews(parent: self)
     }
 }
+
+final class MyCellViewComponents {
+    
+    lazy var poster: UIImageView = {
+        let view = UIImageView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 24)
+        label.textColor = .black
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+//    MARK: exposed func
+    func setupViews(parent: UIView) {
+        parent.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: parent.centerYAnchor),
+            titleLabel.centerXAnchor.constraint(equalTo: parent.centerXAnchor),
+        ])
+    }
+}
+
+
+// MARK: - Extensions
+extension String {
+    @inline(__always) static let empty: String = ""
+}
+
+
+
+
+// MARK: - Playground
+let vc = MyVC()
+vc.preferredContentSize = Design.HIG.preferredContentSize
+
+PlaygroundPage.current.liveView = vc
