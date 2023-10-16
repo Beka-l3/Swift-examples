59009cc1ddf8c75f40964a847ed5b39e069a01a7
Beka-l3 <b.talgat@innopolis.university>
2023-10-16 14:24:53 +0300

    CALayer vs UIView

---
+++ Swift-examples.playground/Pages/CALayer vs UIView.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+
+/// On iOS, every UIView is backed by a Core Animation CALayer, so you are dealing with CALayers when using a UIView, even though you may not realize it.
+/// Unlike NSViews on the Mac, which evolved before Core Animation existed,
+/// UIViews are intended to be lightweight wrappers around these CALayers.
+
+/// working directly with CALayers doesn't give you significant performance advantages over UIViews
+
+/// One of the reasons you might want to build a user interface element with CALayers instead of UIViews is that it can be very easily ported to the Mac.
+/// UIViews are very different from NSViews, but CALayers are almost identical on the two platforms.
+/// This is why the Core Plot framework lays out its graphs using CALayers instead of other UI elements.
+
+/// One thing UIViews provide over CALayers is built-in support for ``user interaction``.
+/// They handle hit-testing on touches and other related actions that you would need to build yourself if managing a hierarchy of CALayers.
+/// It's not that hard to implement this yourself, but it is extra code you'd need to write when building a CALayer-only interface
+
+/// You will often need to access the underlying layers for a UIView when performing more complex animations than the base UIView class allows.
+/// UIView's animation capabilities have grown as the iOS SDK has matured, but there are still a few things that are best done by interacting with the underlying CALayer
+
+/// Simply speaking,`UIView` inherit from ``UIResponder``, handle events from users, contains `CALayer`, which inherit from ``NSObject``, mainly focus on rendering, animation etc
+
+
+/// `UIView`: Views have more `complex` hierarchy layouts.
+/// They can receive user interactions like taps, pinches, cliks and more. Working with UIViews happens on the main thread, it means it is using ``CPU`` power.
+
+/// `CALayer`: Layers on other hand have `simpler` hierarchy.
+/// That means they are faster to resolve and quicker to draw on the screen.
+/// There is no responder chain overhead unlike with views. Layers are drawn directly on the ``GPU``.
+/// It happens on a `separate thread `without burdening the `CPU`.
--- Swift-examples.playground/Pages/CALayer | playground.xcplaygroundpage/Contents.swift
+++ Swift-examples.playground/Pages/CALayer | playground.xcplaygroundpage/Contents.swift
@@ -4,20 +4,48 @@ import PlaygroundSupport
 final class MyViewController: UIViewController {
     
     
-    override func loadView() {
-        let view = UIView()
+//    override func loadView() {
+//        let view = UIView()
+//        view.backgroundColor = .systemPurple
+//        self.view = view
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         view.backgroundColor = .systemPurple
-        self.view = view
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        foo1()
+    }
     
     
     func foo1() {
+        let center = view.center
         
+        let layer1 = CALayer()
+        layer1.backgroundColor = UIColor.black.cgColor
+
+        layer1.frame = .init(
+            origin: .init(x: 50, y: 50),
+            size:  .init(width: 100, height: 100)
+        )
+
+        let myView = UIView()
+        myView.frame = .init(
+            origin: .zero,
+            size:  .init(width: 200, height: 200)
+        )
+        myView.center = .init(x: center.x, y: center.y - 100)
+        myView.backgroundColor = .systemBlue
+        myView.layer.addSublayer(layer1)
+        
+        view.addSubview(myView)
     }
-    
-    
 }
 
-PlaygroundPage.current.liveView = MyViewController()
+let mvc = MyViewController()
+mvc.preferredContentSize = CGSize(width: 200, height: 200)
+PlaygroundPage.current.liveView = mvc
 
