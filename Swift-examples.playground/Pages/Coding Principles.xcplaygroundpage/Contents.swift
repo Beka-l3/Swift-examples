0f6bba74fc351f6633751337a4af7afb49cba362
Beka-l3 <b.talgat@innopolis.university>
2023-10-30 16:24:56 +0300

    Coding principles part 1

---
+++ Swift-examples.playground/Pages/Coding Principles.xcplaygroundpage/Contents.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+
+// MARK: - YAGNI: You Are Not Gonna Need It
+/// Do not write the code - if you think it will be needed later
+
+/// Especiallt during refactoring
+/// Delete those codes, that are no longer needed. Even if they were used in the past.
+/// If it will be need in the future, you can get from the git
+
+
+
+
+// MARK: - DRY: Don't Repeat Yourself
+/// Duplicating a code - is waste of time and resource. You have to keep up to date code in several places at once
+/// Duplication happens because of the lack of the knowledge about the system.
+/// Before coding, it is better to research the system. May be the functionality is already implemented
+
+/// This conept was described in the book "Программист-прагматик: путь от подмастерья к мастеру" by Andy Hunt and Dave Thomas
+/// It is about the `Single Source of Truth` (SSOT):
+/// In information systems design and theory, a single source of truth (SSOT) is the practice of structuring information models and data schema,
+/// which implies that all pieces of data are processed (or edited) in only one place... SSOTs provide reliable, relevant and usable data
+
+
+
+
+// MARK: - KISS: Keep It Simple, Stupid
+/// This principle states that simple systems are sfare and more reliable
+/// Do not reinvent the wheel. Do not use new tool because they are new, but because they are appropriate
+
+
+
+
+// MARK: - Big Design UP Front
+/// Before starting implementation, make sure that everything is clear
+/// Some coders might think that if you do not write code then you are not coder. It is better to think as Developer
+/// Without planning, starting fromo scratch is about to happen several times
+/// Early problem detection is better than adhoc solutions
+
+/// A very common counter-argument is that the cost of solving problems is often less than the cost of planning time.
+/// The fewer errors a user encounters, the better their experience will be. You may not have another chance to deal with these mistakes.
+
+
+
+
+// MARK: SOLID
+/// 
+
