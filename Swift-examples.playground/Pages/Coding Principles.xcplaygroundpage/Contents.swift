bc50b4fd53ff278023213ddc4d9f95ccb447f374
Beka-l3 <b.talgat@innopolis.university>
2023-10-30 16:35:48 +0300

    Coding principles part 2

--- Swift-examples.playground/Pages/Coding Principles.xcplaygroundpage/Contents.swift
+++ Swift-examples.playground/Pages/Coding Principles.xcplaygroundpage/Contents.swift
@@ -44,5 +44,53 @@ import Foundation
 
 
 // MARK: SOLID
-/// 
+/// **Single Responsibility**
+/// Its importance cannot be overstated. Each object, class and method should be responsible for only one thing. If your object/class/method does too much, you'll end up with spaghetti code.
+/// Another side effect of such code is problems with testing. Convoluted functionality is difficult to test.
 
+/// **Open - Close**
+/// Software objects must be open for extension, but closed for modification. The point is that you cannot override methods or classes, simply adding additional functions as needed.
+/// A good way to solve this problem is to use inheritance. JavaScript solves this problem using composition.
+/// A simple rule: if you change an entity to make it extensible, you have violated this principle for the first time.
+
+/// **Liskov Substitution**
+/// This principle states that objects of higher classes should be replaceable with objects of subclasses, and the application should perform as expected when replaced.
+
+/// **Interface Segregation**
+/// Objects should not depend on interfaces that they do not use
+/// The software must be divided into independent parts. Side effects should be kept to a minimum to ensure independence.
+/// Make sure you don't force objects to implement methods they will never need.
+
+/// **Dependency Inversion**
+/// This principle cannot be overemphasized. We must rely on abstractions rather than concrete implementations. Software components should have low coupling and high consistency.
+/// You need to care not about how something works, but about how it works. A simple example is using dates in JavaScript. You can write your own abstraction layer for them.
+/// Then if your source for receiving dates changes, you will need to make changes in one place, and not in a thousand.
+/// Sometimes adding this level of abstraction takes effort, but it pays off in the long run.
+
+
+
+
+// MARK: Avoid Premature Optimization
+/// This practice encourages developers to optimize code before the need for optimization has been proven. I think if you follow KISS or YAGNI you won't get hooked.
+/// Don't get me wrong, anticipating something bad will happen is a good thing. But before you dive into implementation details, make sure these optimizations are actually useful.
+/// A very simple example is scaling. You wouldn't buy 40 servers on the assumption that your new application will become very popular. You will add servers as needed.
+/// Premature optimization can cause delays in the code and therefore increase the time it takes to get features to market.
+/// Many people consider premature optimization to be the root of all evil.
+
+
+
+
+// MARK: Razor Occam
+/// What does this mean in the programming world? Don't create unnecessary entities unnecessarily. Be pragmatic - consider whether they are necessary as they may end up adding complexity to your codebase
+
+
+
+
+
+//MARK: - Summary
+/// These principles are not very complicated. In fact, it is their simplicity that makes them beautiful.
+/// If you're confused, don't try all of them at once. Just try to work consciously and try to gradually incorporate these principles into your work process.
+
+/// Using basic but powerful principles will allow you to become a better programmer and have a clearer understanding of why you do things.
+
+/// If you apply most of the principles intuitively, it's worth thinking about and being aware of why you do things a certain way.
