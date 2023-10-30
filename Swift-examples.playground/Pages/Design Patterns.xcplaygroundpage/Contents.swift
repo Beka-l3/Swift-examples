d3c10b4f5efcb5925572187779ad91054ce45a14
Beka-l3 <b.talgat@innopolis.university>
2023-10-30 15:14:36 +0300

    Design Patterns

---
+++ Swift-examples.playground/Pages/Design Patterns.xcplaygroundpage/Contents.swift
@@ -0,0 +1,108 @@
+import Foundation
+
+
+/// In software engineering, a design pattern is a general repeatable solution to a commonly occurring problem in software design.
+
+/// A `design pattern `isn't a finished design that can be transformed directly into code.
+/// It is a description or template for how to solve a problem that can be used in many different situations
+
+
+
+
+// MARK: - Creational Patterns
+/// These design patterns are all about class instantiation. This pattern can be further divided into class-creation patterns and object-creational patterns.
+/// While class-creation patterns use inheritance effectively in the instantiation process, object-creation patterns use delegation effectively to get the job done.
+
+// MARK: Abstract Factory
+/// Creates an instance of several families of classes
+
+// MARK: Builder
+/// Separates object construction from its representation
+
+// MARK: Factory Method
+/// Creates an instance of several derived classes
+
+// MARK: Object Pool
+/// Avoid expensive acquisition and release of resources by recycling objects that are no longer in use
+
+// MARK: Prototype
+/// A fully initialized instance to be copied or cloned
+
+// MARK: Singletone
+/// A class of which only a single instance can exist
+
+
+
+
+// MARK: - Structural Patterns
+/// These design patterns are all about Class and Object composition.
+/// Structural class-creation patterns use inheritance to compose interfaces.
+/// Structural object-patterns define ways to compose objects to obtain new functionality.
+
+// MARK: Adapter
+/// Match interfaces of different classes
+
+// MARK: Bridge
+/// Separates an object’s interface from its implementation
+
+// MARK: Composite
+/// A tree structure of simple and composite objects
+
+// MARK: Decorator
+/// Add responsibilities to objects dynamically
+
+// MARK: Facade
+/// A single class that represents an entire subsystem
+
+// MARK: Flyweigth
+/// A fine-grained instance used for efficient sharing
+
+// MARK: Private class data
+/// Restricts accessor/mutator access
+
+// MARK: Proxy
+/// An object representing another object
+
+
+
+
+// MARK: - Behavioral Patterns
+/// These design patterns are all about Class's objects communication.
+/// Behavioral patterns are those patterns that are most specifically concerned with communication between objects.
+
+// MARK: Chain of resposibility
+/// A way of passing a request between a chain of objects
+
+// MARK: Command
+/// Encapsulate a command request as an object
+
+// MARK: Interpreter
+/// A way to include language elements in a program
+
+// MARK: Iterator
+/// Sequentially access the elements of a collection
+
+// MARK: Mediator
+/// Defines simplified communication between classes
+
+// MARK: Memento
+/// Capture and restore an object's internal state
+
+// MARK: Null Object
+/// Designed to act as a default value of an object
+
+// MARK: Observer
+/// A way of notifying change to a number of classes
+
+// MARK: State
+/// Alter an object's behavior when its state changes
+
+// MARK: Strategy
+/// Encapsulates an algorithm inside a class
+
+// MARK: Template method
+/// Defer the exact steps of an algorithm to a subclass
+
+// MARK: Visitor
+/// Defines a new operation to a class without change
+
