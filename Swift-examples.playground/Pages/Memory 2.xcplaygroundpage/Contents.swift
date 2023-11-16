50ec8a3440c37208400ea3e1b7a364a90bc5a2a9
Beka-l3 <b.talgat@innopolis.university>
2023-11-16 17:56:19 +0300

    Memory 2

---
+++ Swift-examples.playground/Pages/Memory 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+
+/// Basic unit of information is ``bit`` = 1/0
+/// Traditionally we orgonize them into groups called ``bytes``
+/// `1 byte == 8 bits`
+
+// Memory is a long sequence of bytes
+/// They are located in a defined order
+/// Every byte has it's own `address`
+
+/// We consider memory as a organized ``words`` but not `bytes`
+/// ``Word`` is a faded terminology in Computer Science, but usually this means unit of pointer size
+/// Modern computers has 64-bit CPU.
+/// So `1 word == 8 bytes == 64 bits`
+/// ! That's the amount of information we get in one access to the memory
+
+
+
+
+
