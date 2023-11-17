c7c08b895a5ec7de3660b1963510a06d80fe9553
Beka-l3 <b.talgat@innopolis.university>
2023-11-17 16:42:25 +0300

    Concurrency 2

---
+++ Swift-examples.playground/Pages/Concurrency 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+
+/// Concurrency in iOS
+/// Main Abstract element in concurrency is ``Queues``
+/// Both `GCD` and `Operation` work with ``Queues``
+
+/// `Queue` - First In First Out (FIFO)
+/// They execute `Tasks` / `Closures` in `order`.  Tasks might have different priorities
+
+/// `Queue` can be of 2 types
+/// `Serial` and `Parallel`
+
+
+// MARK: Grand Central Dispatch (GCD)
+
+
+func foo1() {
+    
+}
