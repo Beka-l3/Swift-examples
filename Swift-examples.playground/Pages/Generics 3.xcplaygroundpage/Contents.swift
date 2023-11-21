fdc116a7aae656bb6a555d04c1620129f0f11aee
Beka-l3 <b.talgat@innopolis.university>
2023-11-21 15:48:52 +0300

    Generic Where Clauses

--- Swift-examples.playground/Pages/Generics 3.xcplaygroundpage/Contents.swift
+++ Swift-examples.playground/Pages/Generics 3.xcplaygroundpage/Contents.swift
@@ -154,3 +154,38 @@ func foo5() {
     print([1260.0, 1200.0, 98.6, 37.0].average())
     // Prints "648.9"
 }
+
+
+
+
+// MARK: Contextual Where Clausesin page link
+/// You can write a generic where clause as part of a declaration that doesn’t have its own generic type constraints, when you’re already working in the context of generic types.
+/// For example, you can write a generic where clause on a `subscript` of a generic type or on a `method` in an extension to a generic type.
+/// The Container structure is generic, and the where clauses in the example below specify what type constraints have to be satisfied to make these new methods available on a container.
+extension Container {
+    
+    func average() -> Double where Item == Int {
+        var sum = 0.0
+        
+        for index in 0..<count {
+            sum += Double(self[index])
+        }
+        
+        return sum / Double(count)
+    }
+    
+    func endsWith(_ item: Item) -> Bool where Item: Equatable {
+        return count >= 1 && self[count-1] == item
+    }
+    
+}
+
+func foo6() {
+    let numbers = [1260, 1200, 98, 37]
+    
+    print(numbers.average())
+    // Prints "648.75"
+    
+    print(numbers.endsWith(37))
+    // Prints "true"
+}
