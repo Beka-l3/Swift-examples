122ae4d828a501e892d71cc70127196d2ab5422c
Beka-l3 <b.talgat@innopolis.university>
2023-11-21 15:55:20 +0300

    Generic Subscriptsin page link

--- Swift-examples.playground/Pages/Generics 3.xcplaygroundpage/Contents.swift
+++ Swift-examples.playground/Pages/Generics 3.xcplaygroundpage/Contents.swift
@@ -233,3 +233,23 @@ protocol ComparableContainer: Container where Item: Comparable { }
 
 
 // MARK: Generic Subscriptsin page link
+extension Container2 {
+    
+    subscript<Indices: Sequence>(indices: Indices) -> [Item] where Indices.Iterator.Element == Int {
+        var result: [Item] = []
+        
+        for index in indices {
+            result.append(self[index])
+        }
+        
+        return result
+    }
+}
+
+/// This extension to the Container protocol adds a subscript that takes a sequence of indices and returns an array containing the items at each given index. This generic subscript is constrained as follows:
+/// - The generic parameter Indices in angle brackets has to be a type that conforms to the Sequence protocol from the Swift standard library.
+/// - The subscript takes a single parameter, indices, which is an instance of that Indices type.
+/// - The generic where clause requires that the iterator for the sequence must traverse over elements of type Int. This ensures that the indices in the sequence are the same type as the indices used for a container.
+
+
+
