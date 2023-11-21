72636c213063574c87dffdb1628817a6208c9b32
Beka-l3 <b.talgat@innopolis.university>
2023-11-21 15:25:25 +0300

    Assciated Types Extension

--- Swift-examples.playground/Pages/Generics 2.xcplaygroundpage/Contents.swift
+++ Swift-examples.playground/Pages/Generics 2.xcplaygroundpage/Contents.swift
@@ -109,9 +109,15 @@ struct Stack<Element>: Container {
 /// Swift can therefore infer that Element is the appropriate type to use as the Item for this particular container.
 
 
+// MARK: Generic Extension with Associated type
+/// You can extend an existing type to add conformance to a protocol, as described in Adding Protocol Conformance with an Extension. This includes a protocol with an associated type.
+/// Swift’s Array type already provides an `append(_:)` method, a count property, and a subscript with an Int index to retrieve its elements.
+/// These three capabilities match the requirements of the Container protocol.
+/// This means that you can extend Array to conform to the Container protocol simply by declaring that Array adopts the protocol. You do this with an empty extension
+extension Array: Container {}
 
-
-
+/// Array’s existing `append(_:)` method and subscript enable Swift to infer the appropriate type to use for Item, just as for the generic Stack type above.
+/// After defining this extension, you can use any Array as a Container
 
 
 
