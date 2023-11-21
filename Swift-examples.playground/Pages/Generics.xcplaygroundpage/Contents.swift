0a7006078782f430e5dd572f0178a4e02d7b3903
Beka-l3 <b.talgat@innopolis.university>
2023-11-21 14:46:02 +0300

    Generics Type

--- Swift-examples.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ Swift-examples.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -35,8 +35,41 @@ func foo1() {
 /// You can provide more than one type parameter by writing multiple type parameter names within the angle brackets, separated by commas
 
 
-
-
+// MARK: Generic Type
+func foo2() {
+    // Not Generic: Int Stack
+    struct IntStack {
+        var items: [Int] = []
+        mutating func push(_ item: Int) {
+            items.append(item)
+        }
+        mutating func pop() -> Int {
+            return items.removeLast()
+        }
+    }
+    
+    
+    // Generic Stack
+    struct Stack<Element> {
+        var items: [Element] = []
+        mutating func push(_ item: Element) {
+            items.append(item)
+        }
+        mutating func pop() -> Element {
+            return items.removeLast()
+        }
+    }
+    
+    var stackOfStrings = Stack<String>()
+    stackOfStrings.push("uno")
+    stackOfStrings.push("dos")
+    stackOfStrings.push("tres")
+    stackOfStrings.push("cuatro")
+    // the stack now contains 4 strings
+    
+    let fromTheTop = stackOfStrings.pop()
+    // fromTheTop is equal to "cuatro", and the stack now contains 3 strings
+}
 
 
 
