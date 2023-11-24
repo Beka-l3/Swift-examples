f764bd6e9ed7f41d3281b33299e8c92da9d577df
Beka-l3 <b.talgat@innopolis.university>
2023-11-24 21:15:00 +0300

    Test Definitions

---
+++ Swift-examples.playground/Pages/Tests 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+
+// MARK: Test Definitions
+
+/// `Unit Test`: A test written by a programmer for the purpose of ensuring that the production code does what the programmer expects it to do.
+/// (For the moment we will ignore the notion that unit tests also aid the design, etc.)
+
+/// `Acceptance Test`: A test written by the business for the purpose of ensuring that the production code does what the business expects it to do.
+/// The authors of these tests are business people, or technical people who represent the business. i.e. Business Analysts, and QA.
+
+/// `Integration Test`: A test written by architects and/or technical leads for the purpose of ensuring that a sub-assembly of system components operates correctly.
+/// These are plumbing tests. They are not business rule tests. Their purpose is to confirm that the sub-assembly has been integrated and configured correctly.
+
+/// `System Test`: An integration test written for the purpose of ensuring that the internals of the whole integrated system operate as planned.
+/// These are plumbing tests. They are not business rule tests. Their purpose is to confirm that the system has been integrated and configured correctly.
+
+/// `Micro-test`: A term coined by Mike Hill (@GeePawHill). A unit test written at a very small scope.
+/// The purpose is to test a single function, or small grouping of functions.
+
+/// `Functional Tes`t: A unit test written at a larger scope, with appropriate mocks for slow components.
+
