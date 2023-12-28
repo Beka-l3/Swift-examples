302bb4acc94cebe0e3bc60722929eab1669089e2
Beka-l3 <b.talgat@innopolis.university>
2023-12-28 17:02:47 +0300

    Setup App Coordinator

---
+++ Animation Examples/Animation Examples/App/AppCoordinator.swift
@@ -0,0 +1,37 @@
+//
+//  AppCoordinator.swift
+//  Animation Examples
+//
+//  Created by Bekzhan Talgat on 28.12.2023.
+//
+
+import UIKit
+
+protocol AppCoordinatorForSceneDelegate {
+    func setupScene(with window: UIWindow)
+}
+
+
+final class AppCoordinator {
+    
+    let rootViewController: RootViewController
+    let navigationController: UINavigationController
+    
+        
+    init() {
+        self.rootViewController = .init()
+        self.navigationController = .init(rootViewController: self.rootViewController)
+    }
+    
+    
+    
+}
+
+
+extension AppCoordinator: AppCoordinatorForSceneDelegate {
+    
+    func setupScene(with window: UIWindow) {        
+        window.rootViewController = navigationController
+    }
+    
+}
---
+++ Animation Examples/Animation Examples/ViewControllers/Root/RootViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Animation Examples
+//
+//  Created by Bekzhan Talgat on 28.12.2023.
+//
+
+import UIKit
+
+
+final class RootViewController: UIViewController {
+    
+    let animationViewControllers: [UIViewController] = []
+    
+    
+//    MARK: lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+
+}
+
