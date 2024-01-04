5c87c7ddf550a8c83c16f316dde8eda720c504d3
Beka-l3 <b.talgat@innopolis.university>
2024-01-04 18:19:22 +0300

    Add Fonts

--- Animation Examples/Animation Examples/Utils/Fonts/Fonts.swift
+++ Animation Examples/Animation Examples/Utils/Fonts/Fonts.swift
@@ -23,6 +23,15 @@ extension Fonts {
         static let regular24 = UIFont(name: title, size: 24)
         static let regular36 = UIFont(name: title, size: 36)
         
+        static let titleLarge = UIFont(name: title, size: 31)
+        static let title1 = UIFont(name: title, size: 25)
+        static let title2 = UIFont(name: title, size: 19)
+        static let headline = UIFont(name: title, size: 19)
+        static let body = UIFont(name: title, size: 14)
+        static let callout = UIFont(name: title, size: 13)
+        static let footnote = UIFont(name: title, size: 12)
+        static let caption = UIFont(name: title, size: 11)
+        
     }
     
 }
