ac0df9cb21b001b8d731cdf497a3459cc25f5780
Beka-l3 <b.talgat@innopolis.university>
2024-01-04 18:08:57 +0300

    Add Fonts

---
+++ Animation Examples/Animation Examples/Utils/Fonts/Fonts.swift
@@ -0,0 +1,28 @@
+//
+//  Fonts.swift
+//  Animation Examples
+//
+//  Created by Bekzhan Talgat on 04.01.2024.
+//
+
+import UIKit
+
+
+enum Fonts {
+    
+}
+
+
+extension Fonts {
+    
+    enum Odachi {
+        
+        private static let title = "Odachi"
+        
+        static let regular16 = UIFont(name: title, size: 16)
+        static let regular24 = UIFont(name: title, size: 24)
+        static let regular36 = UIFont(name: title, size: 36)
+        
+    }
+    
+}
---
+++ Animation Examples/Animation Examples/ViewComponents/SplashAnimationView.swift
@@ -0,0 +1,15 @@
+//
+//  SplashAnimationView.swift
+//  Animation Examples
+//
+//  Created by Bekzhan Talgat on 04.01.2024.
+//
+
+import UIKit
+
+
+final class SplashAnimationView: UIView {
+    
+    
+    
+}
