095c4d89a0ec2f1c579ddc58cb491dc4ef68f13e
Beka-l3 <b.talgat@innopolis.university>
2024-01-04 18:26:41 +0300

    Create Splash Screen View

--- Animation Examples/Animation Examples/ViewComponents/SplashAnimationView.swift
+++ Animation Examples/Animation Examples/ViewComponents/SplashAnimationView.swift
@@ -8,8 +8,74 @@
 import UIKit
 
 
+protocol SplashAnimationViewDelegate: AnyObject {
+    func finishedAnimation()
+}
+
+
 final class SplashAnimationView: UIView {
     
+    weak var delegate: SplashAnimationViewDelegate?
     
+    var title: String {
+        didSet {
+            titleLabel.text = title
+        }
+    }
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = title
+        label.font = Fonts.Odachi.titleLarge
+        label.textColor = .white
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var isAnimating: Bool
+    
+    
+//    MARK: lifecycle
+    init(title: String = "Animations") {
+        self.title = title
+        self.isAnimating = false
+        
+        super.init(frame: .zero)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        self.title = ""
+        self.isAnimating = false
+        
+        super.init(coder: coder)
+    }
+    
+    
+    
+//    MARK: private func
+    private func setupView() {
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
+    }
+    
+}
+
+
+extension SplashAnimationView {
+    
+    func startAnimation() {
+        
+    }
+    
+    func stopAnimation() {
+        
+    }
     
 }
