a6b8b5c80725c985f779d4373fed5cf3f64b579e
Beka-l3 <b.talgat@innopolis.university>
2024-01-08 16:58:38 +0300

    Animate navbar style change 4

--- Animation Examples/Animation Examples/ViewComponents/MyNavigationController/Extensions/MyNavigationController+Style.swift
+++ Animation Examples/Animation Examples/ViewComponents/MyNavigationController/Extensions/MyNavigationController+Style.swift
@@ -12,23 +12,20 @@ extension MyNavigationController {
     
     func setLightNavbarStyle(animated: Bool = true) {
         statusBarStyle = .darkContent
-        setNeedsStatusBarAppearanceUpdate()
-        navigationBar.standardAppearance = navbarStyleLight.standard
-        navigationBar.scrollEdgeAppearance = navbarStyleLight.scrollEdge
-        navigationBar.tintColor = navbarStyleLight.tintColor
+        updateNavbarStyle(animated: animated, style: navbarStyleLight)
     }
     
     func setDarkNavbarStyle(animated: Bool = true) {
         statusBarStyle = .lightContent
-        setNeedsStatusBarAppearanceUpdate()
-        navigationBar.standardAppearance = navbarStyleDark.standard
-        navigationBar.scrollEdgeAppearance = navbarStyleDark.scrollEdge
-        navigationBar.tintColor = navbarStyleDark.tintColor
+        updateNavbarStyle(animated: animated, style: navbarStyleDark)
     }
     
     
 //    MARK: private properties
-    private var navbarStyleLight: (standard: UINavigationBarAppearance, scrollEdge: UINavigationBarAppearance, tintColor: UIColor) {
+    
+    private typealias NavbarStyle = (standard: UINavigationBarAppearance, scrollEdge: UINavigationBarAppearance, tintColor: UIColor)
+    
+    private var navbarStyleLight: NavbarStyle {
         let blurEffect = UIBlurEffect(style: .systemUltraThinMaterialLight)
         let largeTitleAttr = [
             NSAttributedString.Key.foregroundColor: UIColor.black,
@@ -55,7 +52,7 @@ extension MyNavigationController {
         return (appearance, largeAppearance, .black)
     }
     
-    private var navbarStyleDark: (standard: UINavigationBarAppearance, scrollEdge: UINavigationBarAppearance, tintColor: UIColor) {
+    private var navbarStyleDark: NavbarStyle {
         let blurEffect = UIBlurEffect(style: .systemUltraThinMaterialDark)
         let largeTitleAttr = [
             NSAttributedString.Key.foregroundColor: UIColor.white,
@@ -80,6 +77,33 @@ extension MyNavigationController {
         largeAppearance.titleTextAttributes = titleAttr
         
         return (appearance, largeAppearance, .white)
+    }
+    
+    
+//    MARK: private func
+    private func updateNavbarStyle(animated: Bool, style: NavbarStyle) {
+        
+        let action = { [unowned self] in
+            self.navigationBar.standardAppearance = style.standard
+            self.navigationBar.scrollEdgeAppearance = style.scrollEdge
+            self.navigationBar.tintColor = style.tintColor
+        }
+        
+        if animated {
+            
+            UIView.animate(withDuration: 0.6) { [unowned self] in
+                self.setNeedsStatusBarAppearanceUpdate()
+            }
+            
+            UIView.transition(with: self.navigationBar, duration: 0.6, animations: action)
+            
+        } else {
+            
+            setNeedsStatusBarAppearanceUpdate()
+            action()
+            
+        }
+        
     }
     
 }
