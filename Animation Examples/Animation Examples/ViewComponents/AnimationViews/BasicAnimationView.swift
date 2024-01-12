a73d60b1aba48582a422764988b28041a4f31f26
Beka-l3 <b.talgat@innopolis.university>
2024-01-12 21:32:44 +0300

    Refactoring

--- Animation Examples/Animation Examples/ViewComponents/AnimationViews/BasicAnimationView.swift
+++ Animation Examples/Animation Examples/ViewComponents/AnimationViews/BasicAnimationView.swift
@@ -22,7 +22,6 @@ final class BasicAnimationView: UIView, AnimationView {
         case none = ""
     }
     
-    
 //    MARK: exposed properties
     let type: BAVType
     let descriptionText: String
@@ -39,6 +38,12 @@ final class BasicAnimationView: UIView, AnimationView {
 //    MARK: private properties
     private var isAnimating: Bool = false
     private lazy var square: UIView = SquareHero.getSquare()
+    
+    private var squreCenterPosition: SquareHero.CenterPositions = .leftCenter
+    private var squreAlphaValue: SquareHero.AlphaValue = .one
+    private var squreTransformSize: SquareHero.TransformSize = .small
+    private var squreTransformRotation: SquareHero.TransformRotation = .left
+    private var squareColor: SquareHero.Color = .purple
     
     private lazy var descriptionLabel: UILabel = {
         let label = UILabel()
@@ -64,40 +69,6 @@ final class BasicAnimationView: UIView, AnimationView {
         self.descriptionText = type.rawValue
         self.isDescriptionHidden = true
         super.init(coder: coder)
-    }
-    
-    
-//    MARK: exposed func
-    func startAnimation() {
-        
-        switch type {
-            
-        case .position:
-            break
-            
-        case .alpha:
-            break
-            
-        case .size:
-            break
-            
-        case .rotation:
-            break
-            
-        case .color:
-            break
-            
-        case .combined1:
-            break
-            
-        case .combined2:
-            break
-            
-        case .none:
-            break
-            
-        }
-        
     }
     
     
@@ -145,6 +116,85 @@ extension BasicAnimationView: UIStyler {
             
         }
         
+    }
+    
+}
+
+
+extension BasicAnimationView {
+    
+    func startAnimation() {
+        guard !isAnimating else { return }
+        
+        isAnimating = true
+        
+        let animation: () -> Void
+        var duration = GConstants.Animation.Duration.AnimationView.fast
+        
+        switch type {
+            
+        case .position:
+            squreCenterPosition = squreCenterPosition == .leftCenter ? .rightCenter : .leftCenter
+            animation = { [unowned self] in
+                self.square.center = self.squreCenterPosition.asPoint
+            }
+            
+        case .alpha:
+            squreAlphaValue = squreAlphaValue == .one ? .zero : .one
+            animation = { [unowned self] in
+                self.square.alpha = self.squreAlphaValue.rawValue
+            }
+            
+        case .size:
+            squreTransformSize = squreTransformSize == .small ? .large : .small
+            animation = { [unowned self] in
+                self.square.transform = self.squreTransformSize == .small ? .identity : CGAffineTransform(scaleX: 1.25, y: 1.25)
+            }
+            
+        case .rotation:
+            squreTransformRotation = squreTransformRotation == .left ? .right : .left
+            animation = { [unowned self] in
+                self.square.transform = self.squreTransformRotation == .left ? CGAffineTransform(rotationAngle: 0) : CGAffineTransform(rotationAngle: 3.14)
+            }
+            
+        case .color:
+            squareColor = squareColor == .purple ? .yellow : .purple
+            animation = { [unowned self] in
+                self.square.backgroundColor = self.squareColor.asUIColor
+            }
+            
+        case .combined1:
+            squreCenterPosition = squreCenterPosition == .leftCenter ? .rightCenter : .leftCenter
+            squreAlphaValue = squreAlphaValue == .one ? .zero : .one
+            squreTransformRotation = squreTransformRotation == .left ? .right : .left
+            duration = GConstants.Animation.Duration.AnimationView.standard
+            animation = { [unowned self] in
+                self.square.center = self.squreCenterPosition.asPoint
+                self.square.alpha = self.squreAlphaValue.rawValue
+                self.square.transform = self.squreTransformRotation == .left ? CGAffineTransform(rotationAngle: 0) : CGAffineTransform(rotationAngle: 3.14)
+            }
+            
+        case .combined2:
+            squreCenterPosition = squreCenterPosition == .leftCenter ? .rightCenter : .leftCenter
+            squareColor = squareColor == .purple ? .yellow : .purple
+            squreTransformSize = squreTransformSize == .small ? .large : .small
+            duration = GConstants.Animation.Duration.AnimationView.standard
+            animation = { [unowned self] in
+                self.square.center = self.squreCenterPosition.asPoint
+                self.square.backgroundColor = self.squareColor.asUIColor
+                self.square.transform = self.squreTransformSize == .small ? .identity : self.square.transform.scaledBy(x: 1.25, y: 1.25)
+            }
+            
+        case .none:
+            animation = { }
+            
+        }
+        
+        if type != .none {
+            UIView.animate(withDuration: duration, animations: animation) { [unowned self] _ in
+                self.isAnimating = false
+            }
+        }
     }
     
 }
